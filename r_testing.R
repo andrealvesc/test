92824f520a1e81fe71d754d07fc8f4500b03e7c7
andrealvesc <andre.a.costa@unesp.br>
2020-05-27 23:08:05 -0300

    aaaa teste

---
+++ r_testing.R
@@ -0,0 +1,5 @@
+usethis::use_git_config(user.name = "andrealvesc",
+                        user.email = "andre.a.costa@unesp.br")
+print("this file was created within RStudio")
+
+print("And now it lives on GitHub")
